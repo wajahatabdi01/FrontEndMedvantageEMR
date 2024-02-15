f6cde4f3fc361927d306670b765d455f495cc084
sabiha0786 <sabiha@criteriontechnologies.com>
2024-02-15 16:20:20 +0530

    sabiha

--- src/PatientPortal/Pages/MessageInbox.js
+++ src/PatientPortal/Pages/MessageInbox.js
@@ -13,18 +13,13 @@ import doctor_profile3 from '../../assets/images/dashboard/doc-profile3.png';
 
 export default function MessageInbox() {
     const [ismsgRead, setismsgRead] = useState(0)
-
-    const handleOnMsgClikc = () => {
-        setismsgRead(1)
-        console.log("success")
-       
+    const handleOnMsgClikc = () => { 
+        setismsgRead(1)       
     }
-
     const handleCloseMsg=()=>{
         setismsgRead(0)
     }
-useEffect(() => {
-   
+useEffect(() => {   
 },[])
 
     return (
@@ -32,19 +27,21 @@ useEffect(() => {
             <section className="main-content mt-5 pt-3">
                 <div className="container-fluid">
                     <div className="row">
-                        <div className={`${ismsgRead === 1 ? 'col-6 custom-animation' : 'col-12 custom-animation'}`}>
+                        <div className={`${ismsgRead === 1 ? 'col-md-6 col-sm-12 ' : 'col-md-12 col-sm-12 custom-animation'}`}>
                             <div className='med-box'>
                                 <div className='inner-content'>
-                                    <div className='inboxheadingmain mt-2'>
+                                    <div className='inboxheadingmain mt-1'>
                                         <div className="inbox-headingg">Inbox Messages<span className='fontt'>(8/55)</span></div><div className='rightt'> <span> <img src={refresh_icon} alt="" /></span> <div><img src={pencil_icon} alt="" /> <button type='button'> Compose</button> </div></div>
                                     </div>
+
+                                   <div className='lft-mesg-part'>
                                     <div className='inboxdiv-striped' onClick={handleOnMsgClikc}>
                                         <div className='d-flex justify-content-between mt-3 ps-1 pt-2'>
                                             <div className='profile-details1'>
                                                 <div className='mt-2'> <input type="checkbox" name="" value="" /></div>
                                                 <div><img src={doctor_profile} alt="" /></div>
                                                 <div className='doctr-profl'>Dr. Vijay Kumar Goel
-                                                    <div><em>Hand Reconstruction Surgery</em></div>
+                                                    <div className='handsurgery'><em>Hand Reconstruction Surgery</em></div>
                                                 </div>
                                             </div>
                                             <div className='profl-dated'><img src={time_profile} /> 02 Nov, 23 05:55:19</div>
@@ -58,7 +55,7 @@ useEffect(() => {
                                                 <div className='mt-2'> <input type="checkbox" name="" value="" /></div>
                                                 <div><img src={doctor_profile2} alt="" /></div>
                                                 <div className='doctr-profl'>Dr. Vijay Kumar Goel
-                                                    <div><em>Hand Reconstruction Surgery</em></div>
+                                                    <div className='handsurgery'><em>Hand Reconstruction Surgery</em></div>
                                                 </div>
                                             </div>
                                             <div className='profl-dated'><img src={time_profile} /> 02 Nov, 23 05:55:19</div>
@@ -72,7 +69,7 @@ useEffect(() => {
                                                 <div className='mt-2'> <input type="checkbox" name="" value="" /></div>
                                                 <div><img src={doctor_profile3} alt="" /></div>
                                                 <div className='doctr-profl'>Dr. Vijay Kumar Goel
-                                                    <div><em>Hand Reconstruction Surgery</em></div>
+                                                    <div className='handsurgery'><em>Hand Reconstruction Surgery</em></div>
                                                 </div>
                                             </div>
                                             <div className='profl-dated'><img src={time_profile} /> 02 Nov, 23 05:55:19</div>
@@ -86,7 +83,7 @@ useEffect(() => {
                                                 <div className='mt-2'> <input type="checkbox" name="" value="" /></div>
                                                 <div><img src={doctor_profile2} alt="" /></div>
                                                 <div className='doctr-profl'>Dr. Vijay Kumar Goel
-                                                    <div><em>Hand Reconstruction Surgery</em></div>
+                                                    <div className='handsurgery'><em>Hand Reconstruction Surgery</em></div>
                                                 </div>
                                             </div>
                                             <div className='profl-dated'><img src={time_profile} /> 02 Nov, 23 05:55:19</div>
@@ -100,7 +97,7 @@ useEffect(() => {
                                                 <div className='mt-2'> <input type="checkbox" name="" value="" /></div>
                                                 <div><img src={doctor_profile} alt="" /></div>
                                                 <div className='doctr-profl'>Dr. Vijay Kumar Goel
-                                                    <div><em>Hand Reconstruction Surgery</em></div>
+                                                    <div className='handsurgery'><em>Hand Reconstruction Surgery</em></div>
                                                 </div>
                                             </div>
                                             <div className='profl-dated'><img src={time_profile} /> 02 Nov, 23 05:55:19</div>
@@ -108,25 +105,67 @@ useEffect(() => {
                                         <div className='profile-txtmtr px-4 ms-5 mt-1 mb-1 pb-1'> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</div>
 
                                     </div>
+                                    <div className='inboxdiv-striped'>
+                                        <div className='d-flex justify-content-between mt-3 ps-1 pt-2'>
+                                            <div className='profile-details1'>
+                                                <div className='mt-2'> <input type="checkbox" name="" value="" /></div>
+                                                <div><img src={doctor_profile2} alt="" /></div>
+                                                <div className='doctr-profl'>Dr. Vijay Kumar Goel
+                                                    <div className='handsurgery'><em>Hand Reconstruction Surgery</em></div>
+                                                </div>
+                                            </div>
+                                            <div className='profl-dated'><img src={time_profile} /> 02 Nov, 23 05:55:19</div>
+                                        </div>
+                                        <div className='profile-txtmtr px-4 ms-5 mt-1 pb-1'> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</div>
+                                    </div>
+                                    <div className='inboxdiv-striped'>
+                                        <div className='d-flex justify-content-between mt-3 ps-1 pt-2'>
+                                            <div className='profile-details1'>
+                                                <div className='mt-2'> <input type="checkbox" name="" value="" /></div>
+                                                <div><img src={doctor_profile2} alt="" /></div>
+                                                <div className='doctr-profl'>Dr. Vijay Kumar Goel
+                                                    <div className='handsurgery'><em>Hand Reconstruction Surgery</em></div>
+                                                </div>
+                                            </div>
+                                            <div className='profl-dated'><img src={time_profile} /> 02 Nov, 23 05:55:19</div>
+                                        </div>
+                                        <div className='profile-txtmtr px-4 ms-5 mt-1 pb-1'> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</div>
+                                    </div>
+                                    <div className='inboxdiv-striped'>
+                                        <div className='d-flex justify-content-between mt-3 ps-1 pt-2'>
+                                            <div className='profile-details1'>
+                                                <div className='mt-2'> <input type="checkbox" name="" value="" /></div>
+                                                <div><img src={doctor_profile2} alt="" /></div>
+                                                <div className='doctr-profl'>Dr. Vijay Kumar Goel
+                                                    <div className='handsurgery'><em>Hand Reconstruction Surgery</em></div>
+                                                </div>
+                                            </div>
+                                            <div className='profl-dated'><img src={time_profile} /> 02 Nov, 23 05:55:19</div>
+                                        </div>
+                                        <div className='profile-txtmtr px-4 ms-5 mt-1 pb-1'> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</div>
+                                    </div>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
                          {ismsgRead === 1 ? (
-                            <div className='col-6 p-0' id='messgpartid' >
+                            <div className='col-md-6 col-sm-12 custom-animation' >
                             <div className='med-box'>
                                 <div className='inner-content'>
+                                <div className='rht-mesg-part'>
                                     <div className='d-flex justify-content-between mt-3 ps-1 pt-2'>
                                         <div className='profile-details1'>
                                         <div className='arowlft mt-2'><i class="bi bi-arrow-left" onClick={handleCloseMsg}></i> </div>
                                             <div><img src={doctor_profile} alt="" /></div>
                                             <div className='doctr-profl'>Dr. Vijay Kumar Goel
-                                                <div><em>Hand Reconstruction Surgery</em></div>
+                                                <div className='handsurgery'><em>Hand Reconstruction Surgery</em></div>
                                             </div>
                                         </div>
-                                        <div className='profl-dated'><img src={time_profile} /> 02 Nov, 23 05:55:19</div>
+                                        <div className='profl-dated ms-2'><img src={time_profile} /> 02 Nov, 23 05:55:19</div>
                                     </div>
                                     <hr></hr>
-                                    <div className='px-2 ps-2'>Sub-Hand Reconstruction Surgery</div>
+                                    <div className='handsurgery1 px-2 ps-2'>Sub- Hand Reconstruction Surgery</div>
                                     <div className='subjectts px-2 mb-2 mt-2'>Hello Shiva Mishra,</div>
                                     <div className='profile-txtmtr px-2 '> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore mag. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore mag.</div>
                                     <div className='profile-txtmtr px-2 mb-4 mt-2 '> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore mag. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</div>
@@ -140,6 +179,7 @@ useEffect(() => {
                                     <div className='messg-btnn'>
                                         <button type="button"><i class="bi-arrow-90deg-left"></i> Reply</button>
                                         <button type="button"><i class="bi-arrow-90deg-right"></i> Farward</button>
+                                    </div>
                                     </div>
 
                                 </div>
