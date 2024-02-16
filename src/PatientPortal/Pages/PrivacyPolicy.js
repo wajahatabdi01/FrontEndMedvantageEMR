e91bef017875d13bac3d1d11730a0e2348d74d69
sabiha0786 <sabiha@criteriontechnologies.com>
2024-02-16 17:06:20 +0530

    Sabiha(PatientPortal)

---
+++ src/PatientPortal/Pages/PrivacyPolicy.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+export default function PrivacyPolicy() {
+    return (
+        <>
+            <section className="main-content mt-5 pt-3">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className='med-box'>
+                                <div className='inner-content'>
+                                    <div class="headingprivacy mt-2 mb-2 pb-0">Privacy Policy
+                                    </div>
+                                    <div className='privacymaidiv'>
+                                        <div className='privacyhdrtext mt-0 pt-0'>Notice of privacy practices patient acknowledgement and consent to medical treatment</div>
+                                        <div className='privacydetails mt-2 mb-2'>
+                                            <div className='lft-details mb-1'> <em>Patient Name  : </em><span className='ptn-dtails'> Shiva Mishra </span></div>
+                                            <div className='lft-details mb-1'> <em>Date of Birth : </em>  <span className='ptn-dtails'> 1967-02-08 </span></div>
+                                        </div>
+                                        <div className='paragraphmain'>
+                                            <div className='privacypara'>
+                                                I have received and understand this practice's Notice of Privacy Practices written in plain English. The notice provides in detail the uses and disclosures of my protected health information that may be made by this practice, my individual rights, how | may exercise those rights, and the practices legal duties with respect to my information.
+                                            </div>
+                                            <div className='privacypara mt-1'>
+                                                I understand that the practice reserves the right to change the terms of the Privacy Practices, and to make changes regarding all protected health information. If changes occur then the practice will provide me with a revised copy upon request.
+                                            </div>
+                                            <div className='privacypara mt-1 mb-1'>
+                                                I voluntarily consent to care, including physician examination and tests such as x-ray, laboratory tests and to medical treatment by my physician or his/her assistants or designees, as may be necessary in the judgment of my physician. No guarantees have been made to me as the result of treatment or examination.
+                                            </div>
+                                        </div>
+                                        <div className='headingpara'>
+                                            <div className='hedingprivacy mt-3'>
+                                                Authorization for:
+                                            </div>
+                                            <div className='privacypara mt-1 mb-1'>
+                                                In consideration for services received by | agree to pay any and all charges as billed. | also request that direct payments be made to on my behalf by insurers and agencies in the settlement of any of my claims. | understand that my protected health information may need to be released for the purpose of treatment, payment or health care operations.
+                                            </div>
+                                            <div className='hedingprivacy mt-3'>
+                                                Medicare Patients:
+                                            </div>
+                                            <div className='privacypara mt-1 mb-1'>
+                                                I certify that the information given by me for application for payment under title XVIII of the Social Security Act is correct. | authorize any holder of medical or other relevant information about me be released to the Social Security Administration or it's intermediaries of carriers and such information needed to support application for payment. Including records pertaining to HIV status or treatment (AIDS records), drug and alcohol treatment, and or psychiatric treatment. | assign and authorize payment directly to for the unpaid charges for the physician's services. | understand that | am responsible for all insurance deductibles and coinsurance.
+                                            </div>
+                                        </div>
+                                        <div className='Commentpart col-xxl-12 col-xl-12 col-lg-12 col-md-12'>
+                                            <div className='leftcomnt col-xxl-4 col-xl-4 col-lg-12 col-md-12'>                                                
+                                                    <div className='privacycomment2 mt-3'> Comments : <div><input type='text' placeholder='Enter Comments' /></div></div>    
+                                                    <div className='privacycomment2'> Do you authorize electronic signature : <span> <input type="checkbox" name="" value="" /></span></div>  
+                                                    <div className='privacycomment2'> Are you Primary Care Giver : <span className='privcyradio'> 
+                                                      <input type="radio" name="" value="" checked />  </span> Yes <span> <input type="radio" name="" value="" /></span> No </div>                                               
+                                                    <div className='privacycomment2'> Clinic Representative Signature Signed : <span> </span></div>                                             
+
+                                            </div>
+                                            <div className="leftcomnt col-xxl-6 col-xl-6 col-lg-12 col-md-12">                                              
+                                                    <div className='privacycomment2 mt-3'>Signature : <span>Saitiyapur Nijampur, Hardoi</span> </div>                                           
+                                                    <div className='privacycomment2'> Relationship to patient (f signed by a personal representative :
+                                                        <div> <input type='text' placeholder='Enter Relationship' /></div></div>      
+                                                    <div className='privacycomment2'> Date : <span> Saitiyapur Nijampur, Hardoi</span></div>
+
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+        </>
+    )
+}
