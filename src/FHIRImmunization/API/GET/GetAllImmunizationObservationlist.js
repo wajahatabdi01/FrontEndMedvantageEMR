a2c2030285b5b4319e117dfdd1ef70ec1350a8c4
WaqarCT <waqar.ahmad@criteriontechnologies.com>
2024-02-28 13:55:51 +0530

    Changes in immunization for edit

---
+++ src/FHIRImmunization/API/GET/GetAllImmunizationObservationlist.js
@@ -0,0 +1,12 @@
+async function GetAllImmunizationObservationList(id) {
+  // let url = window.fhiropenEMR+"/api/FHIRImmunizationMaster/GetAllImmunizationObservationList";
+  let url = window.AppbaseUrl+"/api/FHIRImmunization/GetAllImmunizationObservationData?Id="+id;
+  let head = {"Content-Type":"application/JSON", accept : "*/*"}
+  let data = {}
+  let response = fetch(url, {
+    headers: head,
+    method : 'GET'
+}).then((res) => res.json()).then(data);
+return response;  
+}
+export default GetAllImmunizationObservationList;
